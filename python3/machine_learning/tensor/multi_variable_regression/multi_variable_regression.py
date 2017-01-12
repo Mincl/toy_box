3e37621aaae1b58d201366e735a59fbb6f67ad9e
Mincl <eonchan99@gmail.com>
2017-01-12 21:12:00 +0900

    python3/machine_learning - multi-variable regression practice code

---
+++ python3/machine_learning/tensor/multi_variable_regression/multi_variable_regression.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+x_data = [[0., 2., 0., 4., 0.],
+          [1., 0., 3., 0., 5.]]
+y_data = [1, 2, 3, 4, 5]
+
+W = tf.Variable(tf.random_uniform([1,2], -1.0, 1.0))
+b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+
+hypothesis = tf.matmul(W, x_data) + b
+
+cost = tf.reduce_mean(tf.square(hypothesis - y_data))
+
+a = tf.Variable(0.1)
+optimizer = tf.train.GradientDescentOptimizer(a)
+train = optimizer.minimize(cost)
+
+init = tf.initialize_all_variables()
+
+sess = tf.Session()
+sess.run(init)
+
+for step in range(2001):
+    sess.run(train)
+    if step % 20 == 0:
+        print(step, sess.run(cost), sess.run(W), sess.run(b))

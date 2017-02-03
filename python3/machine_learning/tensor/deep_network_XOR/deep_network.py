dcc6f34334389567f9ec63380f9e4c2c371e3102
Mincl <eonchan99@gmail.com>
2017-02-03 21:43:37 +0900

    python3/machine_learning - add accuracy scalar summary to deep network with TensorBoard

--- python3/machine_learning/tensor/deep_network_XOR/deep_network.py
+++ python3/machine_learning/tensor/deep_network_XOR/deep_network.py
@@ -47,6 +47,10 @@ with tf.name_scope('train') as scope:
     optimizer = tf.train.GradientDescentOptimizer(a)
     train = optimizer.minimize(cost)
 
+correct_prediction = tf.equal(tf.floor(hypothesis+0.5), Y)
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+accuracy_summ = tf.scalar_summary('accuracy', accuracy)
+
 init = tf.initialize_all_variables()
 
 with tf.Session() as sess:
@@ -61,10 +65,8 @@ with tf.Session() as sess:
         if step % 1000 == 0:
             summary = sess.run(merged, feed_dict={X:x_data, Y:y_data})
             writer.add_summary(summary, step)
-            print(step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run(W1), sess.run(W2))
+            print(step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run(W1), sess.run(W2), sess.run(W3))
 
     # Test model
-    correct_prediction = tf.equal(tf.floor(hypothesis+0.5), Y)
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
     print(sess.run([hypothesis, tf.floor(hypothesis+0.5), correct_prediction, accuracy], feed_dict={X:x_data, Y:y_data}))
     print("Accuracy: ", accuracy.eval({X:x_data, Y:y_data}))

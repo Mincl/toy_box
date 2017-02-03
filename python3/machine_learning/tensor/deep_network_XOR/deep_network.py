e89d32fe3c6e06817abe23c4a02c405b1459935e
Mincl <eonchan99@gmail.com>
2017-02-03 21:03:03 +0900

    python3/machine_learning - deep network for XOR problem with TensorFlow

---
+++ python3/machine_learning/tensor/deep_network_XOR/deep_network.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import numpy as np
+
+xy = np.loadtxt('train.txt', unpack=True, dtype='float32')
+x_data = np.transpose(xy[0:-1])
+y_data = np.reshape(xy[-1], (4, 1))
+
+print(x_data)
+print(y_data)
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+W1 = tf.Variable(tf.random_uniform([2, 5], -1.0, 1.0))
+W2 = tf.Variable(tf.random_uniform([5, 4], -1.0, 1.0))
+W3 = tf.Variable(tf.random_uniform([4, 1], -1.0, 1.0))
+
+b1 = tf.Variable(tf.zeros([5]), name="Bias1")
+b2 = tf.Variable(tf.zeros([4]), name="Bias2")
+b3 = tf.Variable(tf.zeros([1]), name="Bias3")
+
+L2 = tf.sigmoid(tf.matmul(X, W1) + b1)
+L3 = tf.sigmoid(tf.matmul(L2, W2) + b2)
+hypothesis = tf.sigmoid(tf.matmul(L3, W3) + b3)
+
+cost = -tf.reduce_mean(Y*tf.log(hypothesis) + (1-Y)*tf.log(1-hypothesis))
+
+a = tf.Variable(0.1)
+optimizer = tf.train.GradientDescentOptimizer(a)
+train = optimizer.minimize(cost)
+
+init = tf.initialize_all_variables()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for step in range(10000):
+        sess.run(train, feed_dict={X:x_data, Y:y_data})
+        if step % 1000 == 0:
+            print(step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run(W1), sess.run(W2))
+
+    # Test model
+    correct_prediction = tf.equal(tf.floor(hypothesis+0.5), Y)
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print(sess.run([hypothesis, tf.floor(hypothesis+0.5), correct_prediction, accuracy], feed_dict={X:x_data, Y:y_data}))
+    print("Accuracy: ", accuracy.eval({X:x_data, Y:y_data}))
---
+++ python3/machine_learning/tensor/deep_network_XOR/logistic_classification.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import numpy as np
+
+xy = np.loadtxt('train.txt', unpack=True, dtype='float32')
+x_data = np.transpose(xy[0:-1])
+y_data = np.reshape(xy[-1], (4,1))
+
+X = tf.placeholder(tf.float32)
+Y = tf.placeholder(tf.float32)
+
+W = tf.Variable(tf.random_uniform([1,len(x_data)], -1.0, 1.0))
+
+h = tf.matmul(W, X)
+hypothesis = tf.div(1., 1.+tf.exp(-h))
+
+cost = -tf.reduce_mean(Y*tf.log(hypothesis) + (1-Y)*tf.log(1-hypothesis))
+
+a = tf.Variable(0.01)
+optimizer = tf.train.GradientDescentOptimizer(a)
+train = optimizer.minimize(cost)
+
+init = tf.initialize_all_variables()
+
+with tf.Session() as sess:
+    sess.run(init)
+
+    for step in range(1000):
+        sess.run(train, feed_dict={X:x_data, Y:y_data})
+        if step % 200 == 0:
+            print(step, sess.run(cost, feed_dict={X:x_data, Y:y_data}), sess.run(W))
+
+    # Test model
+    correct_prediction = tf.equal(tf.floor(hypothesis+0.5), Y)
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    print(sess.run([hypothesis, tf.floor(hypothesis+0.5), correct_prediction, accuracy], feed_dict={X:x_data, Y:y_data}))
+    print("Accuracy: ", accuracy.eval({X:x_data, Y:y_data}))

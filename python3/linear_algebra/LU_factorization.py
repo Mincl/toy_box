977ad2d29d8fff108fb9b62810c9c1ff85a0830c
Mincl <eonchan99@gmail.com>
2017-03-26 19:46:37 +0900

    python3/linear_algebra - advanced LU_factorization, add inverse matrix with gaussian elimination
    include test python script (main.py)
    include file input with numpy (zero_input.txt)

--- python3/linear_algebra/LU_factorization.py
+++ python3/linear_algebra/LU_factorization.py
@@ -1,37 +1,36 @@
 import numpy as np
 
-f = open("input.txt", "r")
+def swap_rows(mat, frm, to):
+    mat[[frm, to], :] = mat[[to, frm], :]
 
-A_str = f.readline()
-C_str = f.readline()
-
-f.close()
-
-A = np.matrix(A_str)
-B = np.matrix(C_str)
-
-def make_LU(A):
+# parameter: target matrix
+# return: L, U, P matrix
+def LU_decomposition(A):
     n = len(A)
-    L = np.identity(n)
-    U = A
+    L = np.zeros((n, n))
+    U = A.copy()
+    P = np.identity(n)
     for i in range(0, n):
+        pivot = U[i, i]
+        if pivot == 0:
+            # find non zero pivot from same cols
+            for j in range(i+1, n):
+                if U[j, i] != 0:
+                    swap_rows(U, i, j)
+                    swap_rows(L, i, j)
+                    swap_rows(P, i, j)
+                    break
+            pivot = U[i, i]
         for j in range(i+1, n):
-            multiple = U[j, i] / U[i, i]
+            multiple = U[j, i] / pivot
             L[j, i] = multiple
             for k in range(0, n):
                 U[j, k] -= multiple * U[i, k]
-    return [L, U]
+    for i in range(0, n):
+        L[i, i] = 1
+    return [L, U, P]
 
-L, U = make_LU(A)
-
-print(L*U)
-print(L)
-print(U)
-print(B)
-
-# A = n * n
-# B = b * 1
-def find_value_by_topdown(L, B):
+def forward_substitution(L, B):
     n = len(L)
     result = np.empty([n, 1])
     for i in range(0, n):
@@ -41,8 +40,8 @@ def find_value_by_topdown(L, B):
         result[i, 0] = B[i] - s
     return result
 
-def find_value_by_bottomup(U, C):
-    n = len(A)
+def backward_substitution(U, C):
+    n = len(U)
     result = np.empty([n, 1])
     for i in range(n-1, -1, -1):
         s = 0
@@ -51,9 +50,51 @@ def find_value_by_bottomup(U, C):
         result[i, 0] = (C[i] - s) / U[i, i]
     return result
 
+def LU_factorization(A, B):
+    L, U, P = LU_decomposition(A)
+    print(L)
+    print(U)
+    print(P)
+    print("")
+    middle = forward_substitution(L, P * B)
+    print(middle)
+    print("")
+    return backward_substitution(U, middle)
 
-C = find_value_by_topdown(L, B)
-print(C)
+def inverse_matrix(A):
+    n = len(A)
+    A2 = A.copy()
+    B = np.identity(n)
+    # forward substitution
+    for i in range(0, n):
+        pivot = A2[i, i]
+        if pivot == 0:
+            # find non zero pivot from same cols
+            for j in range(i+1, n):
+                if A2[j, i] != 0:
+                    swap_rows(A2, i, j)
+                    swap_rows(B, i, j)
+                    break
+            pivot = A2[i, i]
+        for j in range(i+1, n):
+            multiple = A2[j, i] / pivot
+            for k in range(0, n):
+                B[j, k] -= multiple * B[i, k]
+                A2[j, k] -= multiple * A[i, k]
+    # upward substitution
+    for i in range(n-1, -1, -1):
+        pivot = A2[i, i]
+        for j in range(i-1, -1, -1):
+            multiple = A2[j, i] / pivot
+            A2[j, i] = multiple
+            for k in range(0, n):
+                B[j, k] -= multiple * B[i, k]
+    # divide
+    for i in range(0, n):
+        if A2[i, i] != 1:
+            for j in range(0, n):
+                B[i, j] /= A2[i, i]
+            A2[i, i] = 1
+    return B
 
-R = find_value_by_bottomup(U, C)
-print(R)
+
---
+++ python3/linear_algebra/main.py
@@ -0,0 +1,24 @@
+import numpy as np
+from LU_factorization import *
+
+f = open("zero_input.txt", "r")
+
+A_str = f.readline()
+C_str = f.readline()
+
+f.close()
+
+A = np.matrix(A_str)
+B = np.matrix(C_str)
+print(A)
+print(B)
+print("")
+
+R = LU_factorization(A, B)
+print(R)
+print("")
+
+inverse_A = inverse_matrix(A)
+print(inverse_A)
+
+print(A * inverse_A)
